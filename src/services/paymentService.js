9ebb9bd218f336b9fcb3c0ca46142a4b1d82fa02
nguyennguyen111 <nguyenhnde170514@gmail.com>
2026-01-26 17:21:53 +0700

    a

--- src/services/paymentService.js
+++ src/services/paymentService.js
@@ -8,6 +8,11 @@ export const createPayosPayment = (packageId) => {
   return api.post("/api/payment/payos/create", { packageId });
 };
 
+export const confirmPayosPayment = (orderCode) => {
+  return api.get(`/api/payment/payos/confirm?orderCode=${encodeURIComponent(orderCode)}`);
+};
+
 export default {
   createPayosPayment,
+  confirmPayosPayment,
 };
